482fa82c51e4cf53c8d72b6fe09c949672e166ba
UppalavenkataSai <uppalavenkatasai@gmail.com>
2025-09-22 18:11:21 +0530

    Updated scripts across all day folders

--- day_5/functions/03-function-realtime.sh
+++ day_5/functions/03-function-realtime.sh
@@ -2,23 +2,24 @@
 
 # NOTE : run this code in linux or wsl 
 
-DATE=$(date +%F) # here +%F is a format specifier for date command
-SCRIPT_NAME=$(basename "$0")
-LOGFILE="/tmp/${SCRIPT_NAME%.sh}_${DATE}.log"
+DATE=$(date +%F)                                    # here +%F is a format specifier for date command
+SCRIPT_NAME=$(basename "$0")                        # here $0 is the name of the script
+LOGFILE="/tmp/${SCRIPT_NAME%.sh}_${DATE}.log"       # here %.sh is used to remove .sh from the script name
 
-RED="\e[31m"
-GREEN="\e[32m"
-NORMAL="\e[0m"
+RED="\e[31m"                                        # here \e is escape sequence and [31m is the color code for red
+GREEN="\e[32m"                                      # here [32m is the color code for green
+NORMAL="\e[0m"                                      # here [0m is the color code for normal
 
 VALIDATE(){
-   if [ $1 -ne 0 ]; then     # here $1 is the first argument to the function
+   if [ $1 -ne 0 ]; then                            # here $1 is the first argument to the function
       echo -e "$2 ........ $RED Failed $NORMAL"
       exit 1
     else
       echo -e "$2 ........ $GREEN Successful $NORMAL"
    fi
 }
-# $? ------> 0 success , 1 is failure
+
+# $? ------> 0 success , 1 or 1 to 170 is failure
 
 USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
